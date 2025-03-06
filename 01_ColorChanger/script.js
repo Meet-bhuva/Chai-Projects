be73b9d7f5f84ef4c8ad28df2e26c63d127d9016
Meet-bhuva <meetbhuva96241@gmail.com>
2025-03-06 14:32:47 +0000

    1st complete

--- 01_ColorChanger/script.js
+++ 01_ColorChanger/script.js
@@ -1,7 +1,27 @@
-const buttons = document.querySelectorAll('.button')
-const body = document.querySelectorAll('body')
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
 
 buttons.forEach(function (button) {
-    // console.log(button)
+    console.log(button);
+    button.addEventListener('click', function (e) {
+        console.log(e);
+        console.log(e.target.id);
 
-})
+        const targetId = e.target.id
+        switch(targetId){
+            case 'grey' : console.log("Btn 1 clicked");
+            body.style.backgroundColor = targetId
+            break;
+            case 'orange' : console.log("Btn 2 clicked");
+            body.style.backgroundColor = targetId
+            break;
+            case 'blue' : console.log("Btn 3 clicked");
+            body.style.backgroundColor = targetId
+            break;
+            case 'yellow' : console.log("Btn 4 clicked");
+            body.style.backgroundColor = targetId
+            break;
+        }
+
+    });
+});
